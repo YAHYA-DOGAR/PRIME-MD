0c7ce6be0b2d99b5966449081a6d4096582d81fe
Luffyyoung <142199170+Luffyyoung@users.noreply.github.com>
2024-07-04 11:01:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -44,9 +44,9 @@ global.userImages = process.env.USER_IMAGES || "";
 global.waPresence = process.env.WAPRESENCE || "recording";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "false";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 global.autoreaction = process.env.autoreaction || "true";
@@ -56,7 +56,7 @@ global.scan = "https://pair-qr-wasi-md.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMEdYdExEbVlDaXRrWjVFMHI4RzhtTmQvVEhEZG9VYmV0ZzN0WlRGQ2ZIcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieFJnbWhwa0ZzVFBzZkFtUUZpMjZzMDhtb3dhSmV0MUpoQThweG0rc1VCdz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2UFZrenhKR2JOenRBQmJrc21xQm5oRzZkdHUxZTdrdDFYMDhvOHdUTUd3PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI1SEZSM1lBRXVjdHZhbkx2NG9hdkljRTZyd1daSlFnd2h1SUF0Q3h4NDNBPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJDd0RpY3pYSlNRYkUzSFNrUStUdnlzSGRuOXZISEt2RE1NT0Q4V2p3Mms9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNmYmZwbkdFV0pjaHpqSGNJRnpTd0lMb0RnK1BibnJiTkJNTE1kMytPWDA9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0hHVndVdXlRaFRCU0pVZEVJbXpKbnczOUkyT2orUHZsOHFGRFR4ckNHOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUVhHdXNSeEZia2xZam9WZ3BhMDJuMW5YOG5yWkt1bmJLYzBkTlZHdkIyZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjF6NHBjM29aL1BjZFhlQzNyeVlzRXp1U3FwM1BEcTYvclhHRUV0WkxCaTBmbndaTjVwdUJzdDh3aU1NRHJRS243b2ZwV3VidnBxYVFCcmVCaTVhekFnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NDQsImFkdlNlY3JldEtleSI6IlBNV2p5cWRlakh5RTZxMVRkRTk4aWZvSUU2SjZnVHBDQVB0ajhYdm5aM0E9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6Ii1ONXhmUWFUUXBhRUNoMHkwNXdWZmciLCJwaG9uZUlkIjoiYzg2OGJmNzgtY2NlNS00OWQwLThhNzMtMzlkNjBiZThhZjc2IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjFuYVRKS292UVI5UHYxVUp4ZHpaZFBwSENtYz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJqZHpnL1FGWk55aEMzeEVWVmhSQUZDSnBMWFU9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiTVhYRTRLNkMiLCJtZSI6eyJpZCI6IjI1NDc5Nzg5MDA3ODo1MUBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTjIvN2RjRkVKK25tYlFHR0F3Z0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiaXMwc0hWZlN6UmhqL3Z5RkNaSzV5VVJPeTR5VFp4WUJtbHVhZ3VIamNEWT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZEFMK3RFZG5YM2NNZGd1ZW9Bd3ZPY1B1NzBHRlZUWXlhT3FaY3Zqc05JT3VRN1JscWNpQVBUV1F6Y2FYemp4Y2NTRXFMTUhHU1QvWnVvU3VwNDhwQlE9PSIsImRldmljZVNpZ25hdHVyZSI6IkdLaCtnb2FrdDFQMTFobTkySmJRMGpJWmJ1NXpQOStSaTB5Q2NrZEo5UitLUFMyLzRWSGFRU2loYU1HcFdnNllJMUdjNFl0MUpIcSt1WldzTUhuMENRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0Nzk3ODkwMDc4OjUxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQllyTkxCMVgwczBZWS83OGhRbVN1Y2xFVHN1TWsyY1dBWnBibW9MaDQzQTIifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjAwNzkyNzZ9"
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -78,7 +78,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  AUTOREACTION: process.env.AUTOREACTION || process.env.AUTOREACTION || "true",
+  AUTOREACTION: process.env.AUTOREACTION || process.env.AUTOREACTION || "false",
   LANG: (process.env.THEME || "WASI").toUpperCase(),
 };
 global.rank = "updated";
