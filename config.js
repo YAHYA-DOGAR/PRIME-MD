a729740dcb2566ffc16535273a27120932d738f6
Alexandrustefan2 <dorugabrielivascu12@gmail.com>
2024-07-14 20:12:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -39,17 +39,17 @@ global.wlcm = process.env.WELCOME || "false";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "true";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
-global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
 global.userImages = process.env.USER_IMAGES || "";
 global.waPresence = process.env.WAPRESENCE || "recording";
-global.readcmds = process.env.READ_COMMAND || "true";
+global.readcmds = process.env.READ_COMMAND || "false;
 global.readmessage = process.env.READ_MESSAGE || "true";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "true";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
-global.save_status = process.env.AUTO_SAVE_STATUS || "true";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
-global.autoreaction = process.env.autoreaction || "true";
+global.autoreaction = process.env.autoreaction || "false";
 
 global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://pair-qr-wasi-md.onrender.com";
@@ -67,7 +67,7 @@ module.exports = {
   botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
   ownername: process.env.OWNER_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃",
   errorChat: process.env.ERROR_CHAT || "0770811929",
-  KOYEB_API: process.env.KOYEB_API || "true",
+  KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
